ea14bd12c921b50b08dff83f2ebdda819360d76b
Samuel A. Falvo II <kc5tja@arrl.net>
2021-05-29 19:32:04 -0700

    Initial code

---
+++ compiler/compile.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+from s_expression_parser import parse, ParserConfig, Pair, nil
+
+
+class Compiler:
+    def main(self):
+        print("Hello")
+
+
+if __name__ == '__main__':
+    Compiler().main()
+
