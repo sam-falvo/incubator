0e110fc6bdedff451ab420bed8ecf2a8801a3f56
Samuel A. Falvo II <kc5tja@arrl.net>
2021-11-21 10:42:03 -0800

    Basic REPL

--- hierarchical-fs/cli.c
+++ hierarchical-fs/cli.c
@@ -1,9 +1,55 @@
 #include <unistd.h>
+#include <stdbool.h>
+
+#include <stdio.h>
+
+
+#define CMDBUF_LEN 256
+
+
+static void
+banner(void) {
+	puts("Hierarchical File Space Explorer CLI\n\n");
+}
+
+
+static size_t
+readcl(char *buf, size_t len) {
+	size_t actual;
+
+	write(STDOUT_FILENO, "^ ", 2);
+	actual = read(STDIN_FILENO, buf, len);
+	buf[len-1] = 0;
+	if(actual < len) buf[actual] = 0;
+
+	return actual;
+}
+
+
+static bool
+evalcl(char *buf, size_t len) {
+	bool exitrequested = false;
+
+	write(STDOUT_FILENO, buf, len);
+	puts(": Command not supported\n");
+
+	return exitrequested;
+}
 
 
 int
 main(int argc, char *argv[]) {
-	write(0, "Hello world!\n", 13);
+	bool done = false;
+	static char cmdbuffer[CMDBUF_LEN];
+	size_t length;
+
+	banner();
+	while(!done) {
+		length = readcl(cmdbuffer, CMDBUF_LEN);
+		done = evalcl(cmdbuffer, length);
+		// Printing happens as a consequence of executing commands.
+	}
+
 	return 0;
 }
 
