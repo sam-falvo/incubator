52750a162fabecc8bb755da64c3def522689ff46
Samuel A. Falvo II <kc5tja@arrl.net>
2021-11-21 15:20:31 -0800

    Extract arguments string from command line.

--- hierarchical-fs/cli.c
+++ hierarchical-fs/cli.c
@@ -32,16 +32,16 @@ static bool
 evalcl(char *buf, size_t len) {
 	bool exitrequested = false;
 	char *saveptr;
-	char *delim = "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
+	char *delim = "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\x10"
 	              "\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F ";
-	char *cmd;
+	char *cmd, *args;
 
 	cmd = strtok_r(buf, delim, &saveptr);
+	args = buf + strlen(cmd) + 1;
 
 	if(!strcmp(buf, "exit")) exitrequested = true;
 	else {
-		write(STDOUT_FILENO, cmd, strlen(cmd));
-		puts(": Command not supported\n");
+		printf("%s: Command not supported\n  Args: %s\n", cmd, args);
 	}
 
 	return exitrequested;
