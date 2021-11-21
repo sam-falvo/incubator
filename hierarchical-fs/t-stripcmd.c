1d616f4b6756f61045ca9dd7b9ea3ff02bb7df95
Samuel A. Falvo II <kc5tja@arrl.net>
2021-11-21 10:58:49 -0800

    Unit test for stripcmd

---
+++ hierarchical-fs/t-stripcmd.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int
+main(int argc, char *argv[]) {
+	static char *vecs[] = {
+		"hello world\r\n",
+		"hello\tworld\r\n",
+		"hello\nworld to yo too",
+	};
+	static char *exps[] = {
+		"hello world",
+		"hello\tworld",
+		"hello",
+	};
+
+	char buffer[128];
+	int i;
+
+	for(i = 0; i < 3; i++) {
+		strncpy(buffer, vecs[i], 128);
+		buffer[127] = 0;
+
+		stripcmd(buffer, strlen(buffer));
+
+		if(strcmp(buffer, exps[i])) {
+			fprintf(stderr, "Failed case %d\n", i);
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
