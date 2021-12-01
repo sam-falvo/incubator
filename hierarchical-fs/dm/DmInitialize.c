8847d1769a6b8e6dba00f427e8cf18273e8c6e21
Samuel A. Falvo II <kc5tja@arrl.net>
2021-11-30 22:09:51 -0800

    Contribute DmNode in prep for walk functionality.

--- hierarchical-fs/dm/DmInitialize.c
+++ hierarchical-fs/dm/DmInitialize.c
@@ -10,11 +10,33 @@
 DmAnchorBlock *
 DmInitialize(void) {
 	DmAnchorBlock *ab;
+	DmNode *n;
 	char *curdir;
 
 	curdir = malloc(DFLT_CURDIR_LEN);
-	if(!curdir) return NULL;
+	if(!curdir) goto nomem;
+
+	n = malloc(sizeof(DmNode));
+	if(!n) goto nomem;
+
 	strcpy(curdir, "/");
+
+	n->name = curdir;
+	n->qid.type = QTDIR;
+	n->qid.path = 0;
+	n->qid.version = 0;
+	n->atime = 0;	// Root node is ancient.
+	n->mtime = 0;
+	n->length = 0;
+	n->owner = "root";
+	n->group = "root";
+	n->muid = "root";
+	n->parent = n;
+	n->sibling = NULL;
+	n->children = NULL;
+	n->ops = NULL;	// FIXME: will definitely crash if ever it's used.
+	n->nchildren = 0;
+	n->refcnt = 1;
 
 	ab = malloc(sizeof(DmAnchorBlock));
 	if(ab) {
@@ -24,5 +46,10 @@ DmInitialize(void) {
 		ab->curdir = curdir;
 	}
 	return ab;
+
+nomem:
+	if(n) free(n);
+	if(curdir) free(curdir);
+	return NULL;
 }
 
--- hierarchical-fs/include/dm.h
+++ hierarchical-fs/include/dm.h
@@ -1,6 +1,8 @@
 #ifndef DM_DM_H
 #define DM_DM_H
 
+#include <stdint.h>
+
 
 enum {
 	E_OK = 0,
@@ -15,12 +17,74 @@ enum {
 #define E_NOTFOUND	(E_NOTEXISTS)
 
 
-typedef struct DmAnchorBlock DmAnchorBlock;
+typedef struct DmAnchorBlock	DmAnchorBlock;
+typedef struct DmNode		DmNode;
+typedef struct DmQid		DmQid;
+typedef struct DmOps		DmOps;
+
 
 struct DmAnchorBlock {
 	int	curdirlen;
 	char    *curdir;
+	DmNode	*root;
 };
+
+struct DmQid {
+	uint8_t		type;
+	uint32_t	version;
+	uint64_t	path;
+};
+
+
+struct DmOps {
+};
+
+
+struct DmNode {
+	char		*name;
+	DmQid		qid;
+	uint32_t	mode;
+	uint32_t	atime;
+	uint32_t	mtime;
+	uint64_t	length;
+	char		*owner;
+	char		*group;
+	char		*muid;
+	DmNode		*parent;
+	DmNode		*sibling;
+	DmNode		*children;
+	DmOps		*ops;
+	uint32_t	nchildren;
+	uint32_t	refcnt;
+};
+
+
+// These definitions are taken from Plan 9's man pages.
+// See intro(5) and stat(5) for more details.
+
+#define QTDIR		0x80
+#define QTAPPEND	0x40
+#define QTEXCL		0x20
+#define QTreserved	0x10
+#define QTAUTH		0x08
+#define QTTMP		0x04
+
+#define DMDIR		(QTDIR << 24)
+#define DMAPPEND	(QTAPPEND << 24)
+#define DMEXCL		(QTEXCL << 24)
+#define DMreserved	(QTreserved << 24)
+#define DMAUTH		(QTAUTH << 24)
+#define DMTMP		(QTTMP << 24)
+
+#define DMOWNREAD	0x00000100
+#define DMOWNWRITE	0x00000080
+#define DMOWNEXEC	0x00000040
+#define DMGRPREAD	0x00000020
+#define DMGRPWRITE	0x00000010
+#define DMGRPEXEC	0x00000008
+#define DMOTHREAD	0x00000004
+#define DMOTHWRITE	0x00000002
+#define DMOTHEXEC	0x00000001
 
 
 int DmChangeDir(DmAnchorBlock *, char *);
