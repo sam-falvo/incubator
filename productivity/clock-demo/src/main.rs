fa448c7f7f843764956c486ec25bffa050affb05
Samuel A. Falvo II <kc5tja@arrl.net>
2022-09-30 17:15:37 -0700

    Flesh out horizontal lines and filled rectangles.
    In the clock demonstrator,
    
    1. Create a desktop 320x200 in size, suitable for use on a VIC-II chip.
    2. Create function to draw a "paper surface" on the desktop.  I expect
       this to be moved into a support library in the future.
    
    In the Stencil library,
    
    1. Define type synonyms, making it easier to change types later if
       needed.  Unit, Dimension, and Point are now specified.
    2. Refine RustDoc comments to where they're actually useful.
    3. Stencils now record their pixel width and height, instead of just a
       simple span value.  The span is trivially computed from the width.
    4. new_with_dimensions() now takes unsigned width and height values.
       Even though these values are internally represented using signed
       fields for programming convenience, it is not possible to have a
       negative width or height.  Therefore, new_with_dimensions() now
       performs range checks to make sure that overflow cannot occur when
       performing type conversion between signed and unsigned
       representations.
    5. Since new_with_dimensions() can fail, it returns an Option<Stencil>
       instead of a raw Stencil.
    6. horizontal_line() now performs basic clipping to the stencil it's
       drawing to.  filled_rectangle() is expressed in terms of
       horizontal_line, and so inherits this behavior as well.

--- productivity/clock-demo/src/main.rs
+++ productivity/clock-demo/src/main.rs
@@ -1,25 +1,61 @@
 extern crate stencil;
 extern crate sdlstate;
 
-use stencil::Stencil;
+use stencil::{Stencil, Unit, Dimension};
 use sdlstate::SdlState;
 use std::{thread, time};
 
-const W: usize = 320;
-const H: usize = 200;
+const W: Dimension = 320;
+const H: Dimension = 200;
+
+static DESKTOP_PATTERN: [u8; 8] = [
+    0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55,
+];
+
+static BLACK_PATTERN: [u8; 8] = [
+    0, 0, 0, 0, 0, 0, 0, 0,
+];
+
+static WHITE_PATTERN: [u8; 8] = [
+    255, 255, 255, 255, 255, 255, 255, 255,
+];
+
+fn draw_dialog_box(st: &mut Stencil, paper_left: Unit, paper_top: Unit, paper_right: Unit, paper_bottom: Unit) {
+    let border_left = paper_left - 2;
+    let border_top = paper_top - 1;
+    let border_right = paper_right + 2;
+    let border_bottom = paper_bottom + 1;
+
+    let shadow_left = border_left + 2;
+    let shadow_top = border_top + 1;
+    let shadow_right = border_right + 2;
+    let shadow_bottom = border_bottom + 1;
+
+    st.filled_rectangle((shadow_left, shadow_top), (shadow_right, shadow_bottom), &BLACK_PATTERN);
+    st.filled_rectangle((border_left, border_top), (border_right, border_bottom), &BLACK_PATTERN);
+    st.filled_rectangle((paper_left, paper_top), (paper_right, paper_bottom), &WHITE_PATTERN);
+}
 
 fn main() {
     let mut sdl = SdlState::new("Clock Demo", W as u32, H as u32);
-    let mut strip = Stencil::new_with_dimensions(W, H);
-    let desktop_pattern: [u8; 8] = [
-        0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55, 0xAA, 0x55,
-    ];
+    let desktop_opt = Stencil::new_with_dimensions(W as usize, H as usize);
 
-    strip.filled_rectangle((0, 0), (W, H), &desktop_pattern);
+    match desktop_opt {
+        Some(mut desktop) => {
+            desktop.filled_rectangle((0, 0), (W, H), &DESKTOP_PATTERN);
 
-    sdl.paint_with(|ctx| {
-        ctx.paste_stamp_be((0, 0), (W, H), strip.span, (0, 0), strip.borrow_bits());
-    });
+            draw_dialog_box(&mut desktop, 80, 50, 240, 150);
 
-    thread::sleep(time::Duration::new(15, 0));
+            sdl.paint_with(|ctx| {
+                ctx.paste_stamp_be((0, 0), (W as usize, H as usize), desktop.get_span(), (0, 0), desktop.borrow_bits());
+            });
+
+            thread::sleep(time::Duration::new(15, 0));
+        },
+
+        _ => {
+            panic!("Thou dost expect too much.");
+        }
+    };
+
 }
--- productivity/stencil/src/lib.rs
+++ productivity/stencil/src/lib.rs
@@ -46,10 +46,19 @@
 //! In the above example, columns 0 through 5 exist,
 //! but, column 6 does not.
 
+/// A single coordinate component.
+pub type Unit = isize;
+
+/// A coordinate.
+pub type Point = (Unit, Unit);
+
+/// A dimension component.
+pub type Dimension = isize;
+
 /// A Stencil encapsulates a bitmapped image.
 pub struct Stencil {
-    /// How many bytes are necessary to enclose the width of the stencil.
-    pub span: usize,
+    /// (Width, Height) of the stencil, in dots.
+    dimensions: (Dimension, Dimension),
 
     /// The storage for the raw bits of the stencil.
     bits: Vec<u8>,
@@ -60,17 +69,42 @@ static RIGHT_MASKS: [u8; 8] = [ 0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFF, 
 
 impl Stencil {
     /// Create a new stencil with the dimensions (width, height) provided.
-    pub fn new_with_dimensions(width: usize, height: usize) -> Self {
+    /// If the dimensions are inappropriate (e.g., a width which would overflow a signed integer),
+    /// answer with None.  Otherwise, yield a Stencil instance.
+    pub fn new_with_dimensions(width: usize, height: usize) -> Option<Self> {
+        // Confirm that width and height are safe to use with signed-integer fields.
+        // If not, return None.
+
+        let largest_safe_value: usize = Dimension::MAX as usize;
+
+        if width > largest_safe_value {
+            return None;
+        }
+
+        if height > largest_safe_value {
+            return None;
+        }
+
+        // Otherwise, attempt to allocate memory for the bitmap, and record
+        // the width and height.  Return the stencil.
+
         let span = (width + 7) >> 3;
         let size = span * height;
 
         let mut bits = Vec::with_capacity(size);
         bits.resize(size, 0);
 
-        Self {
-            span,
+        Some(Self {
+            dimensions: (width as Dimension, height as Dimension),
             bits,
-        }
+        })
+    }
+
+    /// Answer with the number of bytes a single row of pixels takes in memory.
+    pub fn get_span(&self) -> usize {
+        let width = self.dimensions.0 as usize;
+
+        (width + 7) >> 3
     }
 
     /// Borrow the buffer containing the bitmapped image as a slice of bytes.
@@ -111,7 +145,7 @@ impl Stencil {
     }
 
     /// Draw a filled rectangle with the given pattern.
-    pub fn filled_rectangle(&mut self, upper_left: (usize, usize), lower_right: (usize, usize), pattern: &[u8; 8]) {
+    pub fn filled_rectangle(&mut self, upper_left: Point, lower_right: Point, pattern: &[u8; 8]) {
         let (left, top) = upper_left;
         let (right, bottom) = lower_right;
         let (width, height) = (right - left, bottom - top);
@@ -119,23 +153,48 @@ impl Stencil {
         if (width <= 0) || (height <= 0) { return }
 
         for y in 0..height {
-            self.horizontal_line((left, top + y), right, pattern[y & 7]);
+            self.horizontal_line((left, top + y), right, pattern[(y & 7) as usize]);
         }
     }
 
     /// Draw a horizontal line with the given pattern.
-    pub fn horizontal_line(&mut self, left: (usize, usize), right: usize, pattern: u8) {
-        let (left, top) = left;
+    pub fn horizontal_line(&mut self, left: Point, mut right: Unit, pattern: u8) {
+        let (mut left, top) = left;
+        let (width, height) = self.dimensions;
 
-        if left >= right { return }
+        // Perform basic clipping to the stencil.
+        //
+        // First, make sure the horizontal line is neither above nor below the stencil.
+        if (top < 0) || (top >= height) { return }
+
+        // Next, make sure the left coordinate is to the left of the right coordinate.
+        // Swap them if necessary.
+        if left > right {
+            let t = right;
+            right = left;
+            left = t;
+        }
+
+        // Knowing left <= right, check to see if the line is too far to the left or right to be
+        // visible on the stencil.
+        if (right < 0) || (left >= width) { return }
+
+        // Make sure that the line has a width of at least one pixel.
+        if left == right { return }
+
+        // Constrain the coordinates to the stencil.
+        left = left.max(0);
+        right = right.min(width);
 
         // We know right > left and right-left >= 1.
         // Decrement right to use inclusive coordinates instead of exclusive.
         let right = right - 1;
-        let left_byte = self.span * top + (left >> 3);
-        let right_byte = self.span * top + (right >> 3);
-        let left_mask = LEFT_MASKS[left & 7];
-        let right_mask = RIGHT_MASKS[right & 7];
+        let span = (width + 7) >> 3;
+
+        let left_byte = ((span * top) + (left >> 3)) as usize;
+        let right_byte = ((span * top) + (right >> 3)) as usize;
+        let left_mask = LEFT_MASKS[(left & 7) as usize];
+        let right_mask = RIGHT_MASKS[(right & 7) as usize];
 
         let mut x = left_byte;
         while x <= right_byte {
