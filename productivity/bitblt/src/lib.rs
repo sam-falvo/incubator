bcaf5e301bc1cbc0345295f76afd5980597675c9
Samuel A. Falvo II <kc5tja@arrl.net>
2022-10-14 00:18:59 -0700

    Add RustDocs and line-level functions.

--- productivity/bitblt/src/lib.rs
+++ productivity/bitblt/src/lib.rs
@@ -1,9 +1,184 @@
+//! Bit-blit support for monochrome bitmaps.
+//!
+//! # Bitmaps
+//!
+//! A bitmap is described as a slice of bytes (`[u8]`), a width, a height, and sometimes a "modulo".
+//! The slice of bytes covers a region of memory which encompasses the entire rectangular region of
+//! a bitmap, as shown below.
+//!
+//! ```text
+//! let src: &[u8];
+//!      |
+//! .----'
+//! |
+//! V
+//! +----+====+====+====+====+----+----+----+ .
+//! |  0 #  1 |  2 |  3 |  4 #  5 |  6 |  7 |  |
+//! +----+----+----+----+----+----+----+----+  |
+//! |  8 #  9 | 10 | 11 | 12 # 13 | 14 | 15 |  |
+//! +----+----+----+----+----+----+----+----+   > bitmap height
+//! | 16 # 17 | 18 | 19 | 20 # 21 | 22 | 23 |  |
+//! +----+----+----+----+----+----+----+----+  |
+//! | 24 # 25 | 26 | 27 | 28 # 29 | 30 | 31 |  |
+//! +----+====+====+====+====+----+----+----+ '
+//!      `---------v---------'
+//!           bitmap width
+//! ```
+//!
+//! **NOTE:**
+//! It is easiest if the slice refers directly to the upper-lefthand corner of a bitmap.
+//! This allows you to refer
+//! to subrectangles of the bitmap
+//! without the need for coordinate translation.
+//! However,
+//! as long as you're careful with coordinate translation,
+//! the slice doesn't strictly have to refer to the upper left-hand corner of a bitmap.
+//! The only hard requirement is that the slice encompasses every byte of the rectangle.
+//! 
+//! The *width* indicates how many *bytes* (note: not pixels!) makes up a single raster line of a
+//! bitmap.  In the example above, the width would be 4, since each raster line takes up 4 bytes.
+//!
+//! The *height* indicates how many raster lines of the bitmap we wish to consider in the blit
+//! operation.  In the example above, the height is also 4.
+//!
+//! If you use the lower-level facilities of this library, you'll also need to be aware of
+//! something called a *modulo*.  This is a measure of the number of bytes from the *end* of one
+//! raster line to the *start* of another.  This is what allows the bit-blit library to consider
+//! sub-rectangles of a larger bitmap.
+//!
+//! ```text
+//! +----+====+====+====+====+----+----+----+
+//! |  0 #  1 |  2 |  3 |  4 #  5 |  6 |  7 |  5 + 4 = 9; therefore, the modulo = 4.
+//! +----+----+----+----+----+----+----+----+
+//!                            |
+//! --------.                  `------------
+//!         |
+//!         V
+//! +----+----+----+----+----+----+----+----+
+//! |  8 #  9 | 10 | 11 | 12 # 13 | 14 | 15 |  13 + 4 = 17
+//! +----+----+----+----+----+----+----+----+
+//!                            |
+//! --------.                  `------------
+//!         |
+//!         V
+//! +----+----+----+----+----+----+----+----+
+//! | 16 # 17 | 18 | 19 | 20 # 21 | 22 | 23 |  21 + 4 = 25
+//! +----+----+----+----+----+----+----+----+
+//!                            |
+//! --------.                  `------------
+//!         |
+//!         V
+//! +----+----+----+----+----+----+----+----+
+//! | 24 # 25 | 26 | 27 | 28 # 29 | 30 | 31 |  29 + 4 = 33; however, this falls outside
+//! +----+====+====+====+====+----+----+----+  the sub-rectangle, so we no longer care.
+//! ```
+//!
+//! Note that it is entirely fine for a modulo to be zero;
+//! in this case, each raster line is stored back to back in memory with no intervening gaps.
+//!
+//! Sometimes, you'll see mentioned a *span*.
+//! Spans are just the sum of a width and a modulo,
+//! as illustrated below.
+//!
+//! ```text
+//! +----+====+====+====+====+----+----+----+
+//! |  0 #  1 |  2 |  3 |  4 #  5 |  6 |  7 |  1 + 4 (width) + 4 (modulo) = 9; therefore, the span = 4.
+//! +----+----+----+----+----+----+----+----+
+//!         |                   :
+//!         `------------------- ------------
+//! --------.
+//!         |
+//!         V
+//! +----+----+----+----+----+----+----+----+
+//! |  8 #  9 | 10 | 11 | 12 # 13 | 14 | 15 |
+//! +----+----+----+----+----+----+----+----+
+//!  . . . 
+//! ```
+//!
+//! # What is a Blit?
+//!
+//! The noun, *blit*, refers to the results of combining two or more sources of bitmap data to
+//! yield a resulting destination bitmap.  The verb, *blit*, refers to the *act* of computing the
+//! above result.
+//!
+//! The blitter algorithm implemented in this library considers two bitmap sources: S and D.
+//! The S source is purely an input bitmap.
+//! The D source is not only an input, but also is the destination for all blit operations.
+//! 
+//! Bitmap data from S can be shifted up to seven bits either to the left or to the right.
+//! After shifting, masking is applied to remove unwanted bits.
+//! This mechanism allows the bit-blitter implementation to not only work with sub-rectangles on
+//! byte boundaries, but also on *bit* boundaries as well.
+//!
+//! The D source is used as-is, without any masking or shifting.
+//!
+//! # BlitContext
+//!
+//! In order to use the bit blitter in this library, you'll need to first create a [[BlitContext]]
+//! structure.  This structure contains the working state of the blitter implementation.
+//! A programmer won't typically need to know the implementation details; however,
+//! if the high-level [[blit_rect]] function is not convenient to use,
+//! manipulating the BlitContext structure will be required before invoking the lower-level
+//! functions.
+//!
+//! **NOTE:** If you need to use the lower-level functions, you are at greater risk for API
+//! breakage should the implementation details change.  When and where possible, you'll *really*
+//! want to use [[blit_rect]] for your blitting needs.
+//!
+//! # Blitting More than Bits
+//!
+//! This library is intended for use with monochrome bitmaps.
+//! For color bitmaps, the most convenient representation is through the use of *bit-planes*.
+//! Imagine stacked bitmaps, where each bitmap contributes its own bits to determine a single
+//! pixel's color.
+//! However, it'll be up to the caller to interpret the color bitmaps appropriately.
+//!
+//! ```text
+//!                .----------------------- color 0
+//!               /   .-------------------- color 1
+//!              /   /   .----------------- color 2
+//!             /   /   /   .-------------- color 3
+//!            /   /   /   /   .----------- color 4
+//!           /   /   /   /   /   .-------- color 5
+//!          /   /   /   /   /   /   .----- color 6
+//!         /   /   /   /   /   /   /   .-- color 7
+//!        /   /   /   /   /   /   /   /
+//!     +---+---+---+---+---+---+---+---+---
+//!     | 0 | 0 | 0 | 0 | 1 | 1 | 1 | 1 | ...
+//!   +---+---+---+---+---+---+---+---+---
+//!   | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 | ...
+//! +---+---+---+---+---+---+---+---+---
+//! | 0 | 1 | 0 | 1 | 0 | 1 | 0 | 1 | ...
+//! ```
+//!
+//! However, with clever configuration of the [[BlitContext]] structure,
+//! it can also be made to work with 2bpp or 4bpp chunky pixel maps.
+//! In particular, shifts and masks would need to be aligned to 2- or 4-bit boundaries;
+//! however, it should still work well.
+//!
+//! ```text
+//! +---+---+---+---+---+---+---+---+---
+//! | 0 : 0 | 0 : 1 | 1 : 0 | 1 : 1 | ...
+//! +---+---+---+---+---+---+---+---+---
+//!  `-----' `-----' `-----' `-----'
+//!     |       |       |       |
+//!     |       |       |       `---- color 3
+//!     |       |       `------------ color 2
+//!     |       `-------------------- color 1
+//!     `---------------------------- color 0
+//! ```
+//!
+//! While this blitter library can also be used to blit 8bpp or larger display depths,
+//! one must ask why they'd want to.  At these color depths, it's faster and much simpler
+//! to just shuffle bytes between byte slices directly.
+
 /// Indicates how to combine the source data and destination data
-/// when performing a bitblt operation.  There are sixteen operations
-/// to select from.
+/// when performing a bitblt operation.  If you consider the four minterms
+/// one can combine two sources of data with (!D & !S, !D & S, D & !S, and D & S),
+/// there are sixteen operations to select from.
 ///
-/// Black will always fill destination bits with zeros.
-/// White will always fill destination bits with ones.
+/// Black will always fill destination bits with zeros, as it selects no minterms at all.
+/// White will always fill destination bits with ones, as it selects all of them.
 #[repr(usize)]
 #[derive(Debug)]
 pub enum BlitOp {
@@ -57,6 +232,47 @@ pub struct BlitContext<'a> {
 
     /// The logical operation to apply to the source and destination data.
     pub operation: BlitOp,
+
+    //--- Line-wise Fields
+
+    /// Mask to apply when blitting the first column of a raster line.
+    /// For ascending blits, this will be on the left-hand edge;
+    /// for descending blits, on the right-hand edge.
+    pub s_fc_mask: u8,
+
+    /// Mask to apply when blitting the last column of a raster line.
+    /// For ascending blits, this will be on the right-hand edge;
+    /// for descending blits, on the left-hand edge.
+    pub s_lc_mask: u8,
+}
+
+impl<'a> BlitContext<'a> {
+    /// Create a new BlitContext with reasonable defaults.
+    ///
+    /// Currently, these defaults assume no shift,
+    /// first and last column masks are set to 0xFF,
+    /// the logical operation is set to [[BlitOp]]::Or,
+    /// and all pointers refer to index 0 of their respective bits slices.
+    ///
+    /// These defaults will not be sufficient for most blit operations, however.
+    /// For this reason, you should use functions like [[blit_rect]] to configure
+    /// the context and perform the blit operation in a single step.
+    pub fn new(src: &'a [u8], dst: &'a mut [u8]) -> Self {
+        BlitContext {
+            s_bits: src,
+            s_ptr: 0,
+            s_shift: 0,
+            s_data: 0,
+            s_mask: 0xFF,
+            s_fc_mask: 0xFF,
+            s_lc_mask: 0xFF,
+
+            d_bits: dst,
+            d_ptr: 0,
+
+            operation: BlitOp::Or,
+        }
+    }
 }
 
 /// Given a source and a destination bitmap,
@@ -77,6 +293,7 @@ pub struct BlitContext<'a> {
 /// Write the combined value back to the destination.
 ///
 /// Increment the source and destination pointers.
+#[inline(always)]
 pub fn blit_byte_ascending(bc: &mut BlitContext) {
     let raw_s = bc.s_bits[bc.s_ptr];
     let s = bc.s_mask
@@ -126,6 +343,7 @@ pub fn blit_byte_ascending(bc: &mut BlitContext) {
 /// Write the combined value back to the destination.
 ///
 /// Decrement the source and destination pointers.
+#[inline(always)]
 pub fn blit_byte_descending(bc: &mut BlitContext) {
     let raw_s = bc.s_bits[bc.s_ptr];
     let s = bc.s_mask
@@ -155,6 +373,66 @@ pub fn blit_byte_descending(bc: &mut BlitContext) {
     bc.d_ptr = bc.d_ptr.overflowing_sub(1).0;
     bc.s_ptr = bc.s_ptr.overflowing_sub(1).0;
     bc.s_data = raw_s;
+}
+
+/// Performs a blit operation on a single raster line
+/// (or vector of bytes)
+/// defined as `width` bytes wide.
+///
+/// The source raster line is assumed to start at `bc.s_ptr` in `bc.s_bits`.
+/// The destination raster line is assumed to start at `bc.d_ptr` in `bc.d_bits`.
+/// The blit operation is performed in an ascending order; that is,
+/// pointers increment and source data is shifted to the right (if shifted at all).
+pub fn blit_line_ascending(bc: &mut BlitContext, width: usize) {
+    match width {
+        0 => (),
+        1 => {
+            bc.s_mask = bc.s_fc_mask & bc.s_lc_mask;
+            blit_byte_ascending(bc);
+        }
+        _ => {
+            bc.s_mask = bc.s_fc_mask;
+            blit_byte_ascending(bc);
+
+            bc.s_mask = 0xFF;
+            for _ in 1 .. width - 1 {
+                blit_byte_ascending(bc);
+            }
+
+            bc.s_mask = bc.s_lc_mask;
+            blit_byte_ascending(bc);
+        }
+    }
+}
+
+/// Performs a blit operation on a single raster line
+/// (or vector of bytes)
+/// defined as `width` bytes wide.
+///
+/// The source raster line is assumed to start at `bc.s_ptr` in `bc.s_bits`.
+/// The destination raster line is assumed to start at `bc.d_ptr` in `bc.d_bits`.
+/// The blit operation is performed in a descending order; that is,
+/// pointers decrement and source data is shifted to the left (if shifted at all).
+pub fn blit_line_descending(bc: &mut BlitContext, width: usize) {
+    match width {
+        0 => (),
+        1 => {
+            bc.s_mask = bc.s_fc_mask & bc.s_lc_mask;
+            blit_byte_descending(bc);
+        }
+        _ => {
+            bc.s_mask = bc.s_fc_mask;
+            blit_byte_descending(bc);
+
+            bc.s_mask = 0xFF;
+            for _ in 1 .. width - 1 {
+                blit_byte_descending(bc);
+            }
+
+            bc.s_mask = bc.s_lc_mask;
+            blit_byte_descending(bc);
+        }
+    }
 }
 
 #[cfg(test)]
--- productivity/bitblt/src/tests.rs
+++ productivity/bitblt/src/tests.rs
@@ -1,63 +1,155 @@
-use crate::{BlitOp, BlitContext};
-use crate::{blit_byte_ascending, blit_byte_descending};
-
-// Given that you have a properly initialized BlitContext structure,
-// when you call blit_byte_ascending,
-// then a single byte should be transferred, leaving the BlitContext in a state where subsequent
-// calls to blit_byte_ascending will seamlessly continue the blit operation.
-#[test]
-fn blit_byte_ascending_tracks_source_bytes() {
-    let src: [u8; 1] = [0x21];
-    let mut dst: [u8; 1] = [0x00];
-
-    let mut bc = BlitContext {
-        s_bits: &src,
-        s_ptr: 0,
-        s_shift: 4,
-        s_data: 0x45,
-        s_mask: 0xFF,
-
-        d_bits: &mut dst,
-        d_ptr: 0,
-
-        operation: BlitOp::Or,
+// Working at the level of individual bytes,
+// these operations are the lowest level of abstraction for blitting something.
+mod byte_level {
+    use crate::{BlitOp, BlitContext};
+    use crate::{
+        blit_byte_ascending, blit_byte_descending,
     };
 
-    blit_byte_ascending(&mut bc);
+    // Given that you have a properly initialized BlitContext structure,
+    // when you call blit_byte_ascending,
+    // then a single byte should be transferred, leaving the BlitContext in a state where subsequent
+    // calls to blit_byte_ascending will seamlessly continue the blit operation.
+    #[test]
+    fn blit_byte_ascending_tracks_source_bytes() {
+        let src: [u8; 1] = [0x21];
+        let mut dst: [u8; 1] = [0x00];
 
-    assert_eq!(bc.d_bits[0], 0x52);
-    assert_eq!(bc.s_data, 0x21);
-    assert_eq!(bc.s_ptr, 1);
-    assert_eq!(bc.d_ptr, 1);
+        let mut bc = BlitContext::new(&src, &mut dst);
+        bc.s_data = 0x45;
+        bc.s_shift = 4;
+
+        blit_byte_ascending(&mut bc);
+
+        assert_eq!(bc.d_bits[0], 0x52);
+        assert_eq!(bc.s_data, 0x21);
+        assert_eq!(bc.s_ptr, 1);
+        assert_eq!(bc.d_ptr, 1);
+    }
+
+    // Given that you have a properly initialized BlitContext structure,
+    // when you call blit_byte_descending,
+    // then a single byte should be transferred, leaving the BlitContext in a state where subsequent
+    // calls to blit_byte_descending will seamlessly continue the blit operation.
+    #[test]
+    fn blit_byte_descending_tracks_source_bytes() {
+        let src: [u8; 1] = [0x21];
+        let mut dst: [u8; 1] = [0x00];
+
+        let mut bc = BlitContext::new(&src, &mut dst);
+        bc.s_data = 0x45;
+        bc.s_shift = 4;
+
+        blit_byte_descending(&mut bc);
+
+        assert_eq!(bc.d_bits[0], 0x14);
+        assert_eq!(bc.s_data, 0x21);
+        assert_eq!(bc.s_ptr, !0);
+        assert_eq!(bc.d_ptr, !0);
+    }
 }
 
-// Given that you have a properly initialized BlitContext structure,
-// when you call blit_byte_descending,
-// then a single byte should be transferred, leaving the BlitContext in a state where subsequent
-// calls to blit_byte_descending will seamlessly continue the blit operation.
-#[test]
-fn blit_byte_descending_tracks_source_bytes() {
-    let src: [u8; 1] = [0x21];
-    let mut dst: [u8; 1] = [0x00];
-
-    let mut bc = BlitContext {
-        s_bits: &src,
-        s_ptr: 0,
-        s_shift: 4,
-        s_data: 0x45,
-        s_mask: 0xFF,
-
-        d_bits: &mut dst,
-        d_ptr: 0,
-
-        operation: BlitOp::Or,
+// This abstraction level isn't much higher than working with individual bytes.
+// However, it is convenient for expressing boolean operations to perform on vectors of bytes.
+mod line_level {
+    use crate::{BlitOp, BlitContext};
+    use crate::{
+        blit_line_ascending, blit_line_descending,
     };
 
-    blit_byte_descending(&mut bc);
+    // Given that you have a properly initialized BlitContext structure,
+    // when you try to blit a zero-width line,
+    // then the context should remain unaltered.
+    #[test]
+    fn zero_width_blit_line_is_nop() {
+        let src: [u8; 2] = [0x11, 0x22];
+        let mut dst: [u8; 2] = [0, 0];
 
-    assert_eq!(bc.d_bits[0], 0x14);
-    assert_eq!(bc.s_data, 0x21);
-    assert_eq!(bc.s_ptr, !0);
-    assert_eq!(bc.d_ptr, !0);
+        let mut bc = BlitContext::new(&src, &mut dst);
+        bc.operation = BlitOp::S;
+
+        blit_line_ascending(&mut bc, 0);
+        assert_eq!(bc.d_bits, &[0, 0]);
+        assert_eq!(bc.d_ptr, 0);
+        assert_eq!(bc.s_ptr, 0);
+        assert_eq!(bc.s_data, 0);
+
+        blit_line_descending(&mut bc, 0);
+        assert_eq!(bc.d_bits, &[0, 0]);
+        assert_eq!(bc.d_ptr, 0);
+        assert_eq!(bc.s_ptr, 0);
+        assert_eq!(bc.s_data, 0);
+    }
+
+    // Given that we want to blit a one-byte raster line,
+    // when performing the blit,
+    // then the first- and last-column masks both apply to the source data fetched.
+    // WHY: The source byte fetched is simultaneously the first and last column in the raster line.
+    #[test]
+    fn blit_one_byte_line() {
+        let src: [u8; 2] = [0xFF, 0xFF];
+        let mut dst: [u8; 2] = [0, 0];
+
+        let mut bc = BlitContext::new(&src, &mut dst);
+        bc.s_fc_mask = 0x3F;
+        bc.s_lc_mask = 0xFC;
+        bc.operation = BlitOp::S;
+
+        blit_line_ascending(&mut bc, 1);
+        assert_eq!(bc.d_bits, &[0x3C, 0]);
+        assert_eq!(bc.d_ptr, 1);
+        assert_eq!(bc.s_ptr, 1);
+        assert_eq!(bc.s_data, 0xFF);
+
+        // Remember pointers already point at offset 1.
+        blit_line_descending(&mut bc, 1);
+        assert_eq!(bc.d_bits, &[0x3C, 0x3C]);
+        assert_eq!(bc.d_ptr, 0);
+        assert_eq!(bc.s_ptr, 0);
+        assert_eq!(bc.s_data, 0xFF);
+    }
+
+    // Given that we want to blit a two-byte or wider raster line,
+    // when performing the blit,
+    // then the first- and last-column masks are separated,
+    // and also bits shifted beyond a byte boundary are accounted for in subsequent bytes.
+    #[test]
+    fn blit_three_byte_line() {
+        // Ascending case
+        {
+            let src: [u8; 4] = [0x12, 0x34, 0x56, 0x78];
+            let mut dst: [u8; 4] = [0; 4];
+
+            let mut bc = BlitContext::new(&src, &mut dst);
+            bc.s_shift = 4;
+            bc.s_fc_mask = 0x0F;
+            bc.s_lc_mask = 0xF0;
+
+            blit_line_ascending(&mut bc, 3);
+            assert_eq!(bc.d_bits, &[0x01, 0x23, 0x40, 0x00]);
+            assert_eq!(bc.d_ptr, 3);
+            assert_eq!(bc.s_ptr, 3);
+            assert_eq!(bc.s_data, 0x56);
+        }
+
+        // Descending case
+        {
+            let src: [u8; 4] = [0x12, 0x34, 0x56, 0x78];
+            let mut dst: [u8; 4] = [0; 4];
+
+            let mut bc = BlitContext::new(&src, &mut dst);
+            bc.s_ptr = 3;
+            bc.s_shift = 4;
+            bc.s_fc_mask = 0xF0;
+            bc.s_lc_mask = 0x0F;
+            bc.d_ptr = 2;
+
+            blit_line_descending(&mut bc, 3);
+            assert_eq!(bc.d_bits, &[0x05, 0x67, 0x80, 0x00]);
+            assert_eq!(bc.d_ptr, -1isize as usize);
+            assert_eq!(bc.s_ptr, 0);
+            assert_eq!(bc.s_data, 0x34);
+        }
+    }
 }
 
