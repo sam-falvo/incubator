76c5e481857ce6bc616939b3f017a39ae7d85ce5
Samuel A. Falvo II <kc5tja@arrl.net>
2022-10-15 18:36:52 -0700

    Implement a crude print-text function.

--- productivity/clock-demo/src/main.rs
+++ productivity/clock-demo/src/main.rs
@@ -4,6 +4,7 @@ extern crate stencil;
 use bitblt::{BlitOp, BlitContext, blit_rect};
 use stencil::stencil::Stencil;
 use stencil::types::{Dimension, Unit};
+use stencil::simple_bitmap_font::SimpleBitmapFont;
 
 use sdlstate::SdlState;
 use std::{thread, time};
@@ -85,20 +86,77 @@ fn main() {
 
 use crate::stencil::sysfont_bsw_9::SYSTEM_BITMAP_FONT;
 
-fn demo(desktop: &mut Stencil, frame: usize) {
+fn demo(desktop: &mut Stencil, _frame: usize) {
     let font = SYSTEM_BITMAP_FONT;
-    let desktop_span = ((desktop.dimensions.0 + 7) >> 3) as usize;
-    let mut bc = BlitContext::new(font.bits, font.span, &mut desktop.bits, desktop_span);
-    let x = (frame & 31) as Unit;
-    let left_to_right = (frame & 32) != 0;
+    let op = BlitOp::Xor;
 
-    let left_a = font.left_edges[65-32] as Unit;
-    let right_a = font.left_edges[66-32] as Unit;
+    let _ = paint_text(desktop, op, &font, 84, 60, "Hello world!").unwrap();
+}
 
-    if left_to_right {
-        blit_rect(&mut bc, left_a as usize, 0, right_a as usize, 9, (160 + x) as usize, 100, BlitOp::DandNotS);
-    } else {
-        blit_rect(&mut bc, left_a as usize, 0, right_a as usize, 9, (160 - x) as usize, 100, BlitOp::DandNotS);
+fn paint_text(stencil: &mut Stencil, op: BlitOp, font: &SimpleBitmapFont, mut x: Unit, y: Unit, text: &str) -> Option<Unit> {
+    for b in text.bytes() {
+        let new_x_opt = paint_char(stencil, op, font, x, y, b as u8);
+        if let Some(new_x) = new_x_opt {
+            x = new_x;
+        } else {
+            return None;
+        }
     }
+    Some(x)
+}
+
+// Try to paint a character onto the stencil.  If not able to do so, answer with None.  Otherwise,
+// an updated X-coordinate is returned.
+fn paint_char(stencil: &mut Stencil, op: BlitOp, font: &SimpleBitmapFont, x: Unit, y: Unit, mut char: u8) -> Option<Unit> {
+    // If not representable in the glyph set of the font, assume the undefined character glyph,
+    // which by definition, is always at highest_char+1 mod 256.
+    let highest_character = font.highest_char;
+    if (char < font.lowest_char) || (char > highest_character) {
+        char = highest_character.overflowing_add(1).0;
+    }
+    char -= font.lowest_char;
+
+    // Let's expand this to a valid array index.
+    let char = char as usize;
+
+    let left_edge = font.left_edges[char];
+    let right_edge = font.left_edges[char + 1];
+    let glyph_width = right_edge - left_edge;
+
+    // Check to see if we can print this character on the stencil.
+    // If not, return None.
+    let stencil_width = stencil.dimensions.0;
+    let new_cursor_position = x + glyph_width as Unit;
+    if new_cursor_position > stencil_width {
+        return None;
+    }
+
+    // Blit the character glyph onto the stencil.
+    //
+    // Because of a bug in bitblt that I do not know how to resolve yet, we must do this in two
+    // steps.  First, we must blit the glyph into a temporary holding stencil that is sized large
+    // enough for the glyph, but which holds the *pre-shifted* version of the glyph.
+
+    let x_in_byte = (x & 7) as usize;
+
+    let mut tmp_buf: [u8; 48] = [0; 48]; // Big enough for fonts up to 16 pixels tall, and with characters no wider than 16px.
+    let mut bc = BlitContext::new(font.bits, font.span, &mut tmp_buf, 3);
+    blit_rect(&mut bc, left_edge as usize, 0, right_edge as usize, font.height as usize, x_in_byte, 0, BlitOp::S);
+
+
+    // Next, blit the pre-shifted glyph onto the destination bitmap with the operation requested by
+    // the caller.  This requires resetting the BlitContext to refer to the correct stencil
+    // bitmaps.
+    bc.s_bits = &(*bc.d_bits);
+    bc.s_span = 3;
+
+    bc.d_bits = &mut stencil.bits;
+    bc.d_span = ((stencil_width + 7) >> 3) as usize;
+
+    let top = (y - font.baseline) as usize;
+    blit_rect(&mut bc, x_in_byte, 0, x_in_byte + glyph_width as usize, font.height as usize, x as usize, top, op);
+
+    // Return the next glyph location.
+    Some(new_cursor_position)
 }
 
