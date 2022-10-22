6c05dfc3bcdae0bc910efabd05bb3854f654e1ed
Samuel A. Falvo II <kc5tja@arrl.net>
2022-10-22 16:25:29 -0700

    introduce the Draw trait.
    Before this patch, the various drawing operations on a stencil are
    tightly bound to the Stencil type.  After this patch, any Stencil-like
    thing could implement a Draw trait to facilitate different kinds of
    stencils.
    
    The motivation for this patch was an ill-fated "AutoStencil" type which
    was intended to update the SDL frame buffer after every Draw operation.
    It turns out that this would not type check (for some reason, and
    frankly, I'm just too lazy to care at as of this commit).  So, I got rid
    of the AutoStencil and I'm back to just normal stencils.  However, I
    kept the trait in anticipation that I'll revisit this design choice
    later.

--- productivity/stencil/src/stencil.rs
+++ productivity/stencil/src/stencil.rs
@@ -1,5 +1,7 @@
 //! Supports immediate-mode drawing into off-screen bitmaps, called stencils.
 //!
+//! # Stencils
+//!
 //! Stencils are off-screen, monochrome bitmaps.
 //! They are primarily intended
 //! to be used as masks
@@ -46,7 +48,29 @@
 //! In the above example, columns 0 through 5 exist,
 //! but, column 6 does not.
 
-use crate::types::{Unit, Point, Dimension};
+use std::mem;
+use crate::types::{Unit, Point, Dimension, Rect};
+
+/// A pattern is an 8x8 pixel tile.
+pub type Pattern = [u8; 8];
+
+/// Stencil and stencil-like types can support Draw to offer a basic set of drawing primitives.
+pub trait Draw {
+    /// Draw a filled rectangle with the given pattern.
+    /// 
+    /// If the right coordinate of the rectangle falls to the left of the left edge,
+    /// or if the left coordinate falls to the right of the right edge, the left and right
+    /// coordinates will be swapped.  A similar coordinate swap for the top and bottom edges also
+    /// takes place.
+    ///
+    /// The upper_left and lower_right coordinates are clipped to the stencil as necessary.
+    fn filled_rectangle(&mut self, upper_left: Point, lower_right: Point, pattern: &Pattern);
+
+    /// Draw a horizontal line with the given pattern.
+    ///
+    /// The left point and right coordinate are clipped to the stencil as necessary.
+    fn horizontal_line(&mut self, left: Point, right: Unit, pattern: u8);
+}
 
 /// A Stencil encapsulates a bitmapped image.
 pub struct Stencil {
@@ -63,35 +87,42 @@ static RIGHT_MASKS: [u8; 8] = [ 0x80, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC, 0xFE, 0xFF, 
 impl Stencil {
     /// Create a new stencil with the dimensions (width, height) provided.
     /// If the dimensions are inappropriate (e.g., a width which would overflow a signed integer),
+    /// or if insufficient memory is available to hold the bitmap,
+    /// panic.
+    pub fn new_with_dimensions(width: Dimension, height: Dimension) -> Self {
+        Stencil::try_new_with_dimensions(width, height).expect("Stencil creation failure")
+    }
+
+    /// Create a new stencil with the dimensions (width, height) provided.
+    /// If the dimensions are inappropriate (e.g., a width which would overflow a signed integer),
     /// answer with None.  Otherwise, yield a Stencil instance.
-    pub fn new_with_dimensions(width: usize, height: usize) -> Option<Self> {
+    ///
+    /// # Panics
+    ///
+    /// This function can panic if it runs out of heap memory.
+    pub fn try_new_with_dimensions(width: Dimension, height: Dimension) -> Option<Self> {
         // Confirm that width and height are safe to use with signed-integer fields.
         // If not, return None.
 
-        let largest_safe_value: usize = Dimension::MAX as usize;
+        if (width > 0) && (height > 0) {
+            // Otherwise, attempt to allocate memory for the bitmap, and record
+            // the width and height.  Return the stencil.
 
-        if width > largest_safe_value {
-            return None;
+            let span = (width + 7) >> 3;
+            let size = (span * height) as usize;
+
+            let mut bits = Vec::with_capacity(size);
+            bits.resize(size, 0);
+
+            Some(Self {
+                dimensions: (width as Dimension, height as Dimension),
+                bits,
+            })
+        } else {
+            None
         }
-
-        if height > largest_safe_value {
-            return None;
-        }
-
-        // Otherwise, attempt to allocate memory for the bitmap, and record
-        // the width and height.  Return the stencil.
-
-        let span = (width + 7) >> 3;
-        let size = span * height;
-
-        let mut bits = Vec::with_capacity(size);
-        bits.resize(size, 0);
-
-        Some(Self {
-            dimensions: (width as Dimension, height as Dimension),
-            bits,
-        })
     }
+
 
     /// Answer with the number of bytes a single row of pixels takes in memory.
     pub fn get_span(&self) -> usize {
@@ -136,9 +167,91 @@ impl Stencil {
     pub fn borrow_bits(&self) -> &[u8] {
         &self.bits
     }
+}
 
+/// Canonize a rectangle's coordinates.
+fn canonize_rectangle(upper_left: Point, lower_right: Point) -> (Point, Point) {
+    let (mut left, mut top) = upper_left;
+    let (mut right, mut bottom) = lower_right;
+
+    if left > right {
+        mem::swap(&mut left, &mut right);
+    }
+
+    if top > bottom {
+        mem::swap(&mut top, &mut bottom);
+    }
+
+    ((left, top), (right, bottom))
+}
+
+#[cfg(test)]
+mod canonize_rectangle_tests {
+    use super::canonize_rectangle;
+
+    #[test]
+    fn identity() {
+        let (lt, rb) = canonize_rectangle((10, 20), (30, 40));
+        assert_eq!(lt, (10, 20));
+        assert_eq!(rb, (30, 40));
+    }
+
+    #[test]
+    fn left_right_swap() {
+        let (lt, rb) = canonize_rectangle((30, 20), (10, 40));
+        assert_eq!(lt, (10, 20));
+        assert_eq!(rb, (30, 40));
+    }
+
+    #[test]
+    fn top_bottom_swap() {
+        let (lt, rb) = canonize_rectangle((10, 40), (30, 20));
+        assert_eq!(lt, (10, 20));
+        assert_eq!(rb, (30, 40));
+    }
+
+    #[test]
+    fn coords_swap() {
+        let (lt, rb) = canonize_rectangle((30, 40), (10, 20));
+        assert_eq!(lt, (10, 20));
+        assert_eq!(rb, (30, 40));
+    }
+}
+
+/// Canonize a horizontal line's coordinates.
+fn canonize_hline(left_pt: Point, mut right: Unit) -> (Point, Unit) {
+    let (mut left, top) = left_pt;
+
+    if left > right {
+        mem::swap(&mut left, &mut right);
+    }
+
+    ((left, top), right)
+}
+
+#[cfg(test)]
+mod canonize_hline_tests {
+    use super::canonize_hline;
+
+    #[test]
+    fn identity() {
+        let (lt, r) = canonize_hline((10, 20), 30);
+        assert_eq!(lt, (10, 20));
+        assert_eq!(r, 30);
+    }
+
+    #[test]
+    fn swap() {
+        let (lt, r) = canonize_hline((30, 20), 10);
+        assert_eq!(lt, (10, 20));
+        assert_eq!(r, 30);
+    }
+}
+
+impl Draw for Stencil {
     /// Draw a filled rectangle with the given pattern.
-    pub fn filled_rectangle(&mut self, upper_left: Point, lower_right: Point, pattern: &[u8; 8]) {
+    fn filled_rectangle(&mut self, upper_left: Point, lower_right: Point, pattern: &[u8; 8]) {
+        let (upper_left, lower_right) = canonize_rectangle(upper_left, lower_right);
         let (left, top) = upper_left;
         let (right, bottom) = lower_right;
         let (width, height) = (right - left, bottom - top);
@@ -151,22 +264,15 @@ impl Stencil {
     }
 
     /// Draw a horizontal line with the given pattern.
-    pub fn horizontal_line(&mut self, left: Point, mut right: Unit, pattern: u8) {
-        let (mut left, top) = left;
+    fn horizontal_line(&mut self, left_pt: Point, right: Unit, pattern: u8) {
+        let (left_pt, mut right) = canonize_hline(left_pt, right);
+        let (mut left, top) = left_pt;
         let (width, height) = self.dimensions;
 
         // Perform basic clipping to the stencil.
         //
         // First, make sure the horizontal line is neither above nor below the stencil.
         if (top < 0) || (top >= height) { return }
-
-        // Next, make sure the left coordinate is to the left of the right coordinate.
-        // Swap them if necessary.
-        if left > right {
-            let t = right;
-            right = left;
-            left = t;
-        }
 
         // Knowing left <= right, check to see if the line is too far to the left or right to be
         // visible on the stencil.
