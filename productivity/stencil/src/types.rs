d3f0e583b1046bb1ecc33e52e576533c65860cd9
Samuel A. Falvo II <kc5tja@arrl.net>
2022-10-22 16:24:34 -0700

    add Rect type alias.

--- productivity/stencil/src/types.rs
+++ productivity/stencil/src/types.rs
@@ -4,6 +4,9 @@ pub type Unit = isize;
 /// A coordinate.
 pub type Point = (Unit, Unit);
 
+/// A rectangle in ((left, top), (right, bottom)) format.
+pub type Rect = (Point, Point);
+
 /// A dimension component.
 pub type Dimension = isize;
 
