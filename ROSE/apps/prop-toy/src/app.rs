f9967d95bf9c1e3f60bb587cce3d2c9997da4396
Samuel A. Falvo II <kc5tja@arrl.net>
2022-10-29 20:51:00 -0700

    Remove all PAC-related work.
    PAC maps well to a Rust environment; however, we still run into
    situations where it just depends too heavily on OO-style polymorphism.
    Also, keeping track of which controller is 'hot' proves problematic.  I
    suspect these are solvable problems; however, I also feel there are
    simpler approaches to what I'm attempting to achieve.

--- ROSE/apps/prop-toy/src/app.rs
+++ ROSE/apps/prop-toy/src/app.rs
@@ -1,22 +1,24 @@
-use std::rc::Rc;
-use std::cell::RefCell;
-
 use stencil::utils::draw_desktop;
 use stencil::stencil::Stencil;
 use stencil::types::{Dimensions, Point, Rect};
 
-/// The root controller shows the desktop background, and keeps track of the mouse pointer.
-/// The root presenter is given by the root environment.
-/// There is no defined root model per se; it is whatever your app needs it to be.
 pub struct ToyState {
     mouse: MouseState,
-    subcontrols: Vec<Rc<RefCell<PushButton<bool>>>>,
-    hot: Option<Rc<RefCell<PushButton<bool>>>>,
 }
 
-pub trait RootController: MouseEventSink + Controller + AppController { }
+pub trait RootController: MouseEventSink + AppController + View { }
 
 impl RootController for ToyState { }
+
+pub trait View {
+    fn draw(&mut self, stencil: &mut Stencil);
+}
+
+impl View for ToyState {
+    fn draw(&mut self, s: &mut Stencil) {
+        draw_desktop(s);
+    }
+}
 
 pub trait AppController {
     fn request_quit(&self) -> bool;
@@ -31,20 +33,6 @@ impl AppController for ToyState {
 impl MouseEventSink for ToyState {
     fn pointer_moved(&mut self, p: Point) {
         self.mouse.xy = p;
-        match self.hot.clone() {
-            None => {
-                self.hot = self.find_enclosing_control(p);
-                if let Some(_) = self.hot {
-                    eprintln!("New hotness");
-                }
-            },
-            Some(h) => {
-                if !h.borrow().has_point(p) {
-                    self.hot = self.find_enclosing_control(p);
-                    eprintln!("From old hotness, we get a new hotness.");
-                } 
-            },
-        }
     }
 
     fn button_down(&mut self) {
@@ -56,42 +44,11 @@ impl MouseEventSink for ToyState {
     }
 }
 
-impl Controller for ToyState {
-    fn draw(&mut self, desktop: &mut Stencil) {
-        draw_desktop(desktop);
-
-        for sc_refcell in &self.subcontrols {
-            sc_refcell.borrow_mut().draw(desktop);
-        }
-    }
-
-    fn has_point(&self, _: Point) -> bool {
-        // We're full-screen, so yes.
-        true
-    }
-}
-
 impl ToyState {
     pub fn new(_display_dimensions: Dimensions) -> Self {
         Self {
             mouse: MouseState::new(),
-            subcontrols: vec![
-                Rc::new(RefCell::new(PushButton::new(((8, 8), (72, 28)), false))),
-                Rc::new(RefCell::new(PushButton::new(((80, 8), (144, 28)), true))),
-            ],
-            hot: None,
         }
-    }
-
-    fn find_enclosing_control(&self, p: Point) -> Option<Rc<RefCell<PushButton<bool>>>> {
-        for subctl in &self.subcontrols {
-            let sc = subctl.borrow();
-
-            if sc.has_point(p) {
-                return Some(subctl.clone());
-            }
-        }
-        None
     }
 }
 
@@ -130,136 +87,6 @@ pub fn init_root(display_dimensions: Dimensions) -> Box<dyn RootController> {
 }
 
 // ------------------------------------------------------------------------
-// Boolean gadget components
-
-// // Presentation
-
-use stencil::stencil::Draw;
-use stencil::utils::{LINE_BLACK, LINE_WHITE, BLACK_PATTERN, WHITE_PATTERN};
-
-struct PushButtonView {
-    area: Rect,
-}
-
-impl PushButtonView {
-    fn new(area: Rect) -> Self {
-        Self {
-            area,
-        }
-    }
-
-    fn get_area(&self) -> Rect {
-        self.area
-    }
-
-    fn draw_unpressed(&self, s: &mut Stencil) {
-        let ((btn_left, btn_top), (btn_right, btn_bottom)) = self.area;
-        let border_left = btn_left;
-        let border_top = btn_top;
-        let border_right = btn_right - 1;
-        let border_bottom = btn_bottom - 1;
-
-        let r_shadow_left = btn_right - 1;
-        let r_shadow_top = btn_top + 1;
-        let r_shadow_bottom = btn_bottom;
-
-        let b_shadow_left = btn_left + 1;
-        let b_shadow_top = btn_bottom - 1;
-        let b_shadow_right = btn_right;
-
-        let subview_left = btn_left;
-        let subview_top = btn_top;
-        let subview_right = btn_right - 1;
-        let subview_bottom = btn_bottom - 1;
-
-        s.filled_rectangle((subview_left, subview_top), (subview_right, subview_bottom), &WHITE_PATTERN);
-        s.framed_rectangle((border_left, border_top), (border_right, border_bottom), LINE_BLACK);
-        s.horizontal_line((b_shadow_left, b_shadow_top), b_shadow_right, LINE_BLACK);
-        s.vertical_line((r_shadow_left, r_shadow_top), r_shadow_bottom, LINE_BLACK);
-    }
-
-    fn draw_pressed(&self, s: &mut Stencil) {
-        let ((btn_left, btn_top), (btn_right, btn_bottom)) = self.area;
-        let border_left = btn_left + 1;
-        let border_top = btn_top + 1;
-        let border_right = btn_right;
-        let border_bottom = btn_bottom;
-
-        let l_shadow_left = btn_left;
-        let l_shadow_top = btn_top;
-        let l_shadow_bottom = btn_bottom;
-
-        let t_shadow_left = btn_left;
-        let t_shadow_top = btn_top;
-        let t_shadow_right = btn_right;
-
-        let subview_left = btn_left + 2;
-        let subview_top = btn_top + 2;
-        let subview_right = btn_right - 1;
-        let subview_bottom = btn_bottom - 1;
-
-        s.filled_rectangle((subview_left, subview_top), (subview_right, subview_bottom), &BLACK_PATTERN);
-        s.framed_rectangle((border_left, border_top), (border_right, border_bottom), LINE_WHITE);
-        s.horizontal_line((t_shadow_left, t_shadow_top), t_shadow_right, LINE_WHITE);
-        s.vertical_line((l_shadow_left, l_shadow_top), l_shadow_bottom, LINE_WHITE);
-    }
-}
-
-// // Abstraction
-
-pub trait BoolViewAbstraction {
-    fn is_selected(&self) -> bool;
-}
-
-impl BoolViewAbstraction for bool {
-    fn is_selected(&self) -> bool {
-        *self == true
-    }
-}
-
-// // Controller
-
-pub trait Controller {
-    fn draw(&mut self, s: &mut Stencil);
-    fn has_point(&self, p: Point) -> bool;
-}
-
-struct PushButton<A: BoolViewAbstraction> {
-    view: PushButtonView,
-    abstraction: A,
-}
-
-impl<A: BoolViewAbstraction> Controller for PushButton<A> {
-    fn draw(&mut self, s: &mut Stencil) {
-        if self.abstraction.is_selected() {
-            self.view.draw_pressed(s);
-        } else {
-            self.view.draw_unpressed(s);
-        }
-    }
-
-    fn has_point(&self, p: Point) -> bool {
-        is_point_within(p, self.view.get_area())
-    }
-}
-
-fn is_point_within(p: Point, r: Rect) -> bool {
-    let (x, y) = p;
-    let ((left, top), (right, bottom)) = r;
-
-    (left <= x) && (x < right) && (top <= y) && (y < bottom)
-}
-
-impl<A: BoolViewAbstraction> PushButton<A> {
-    fn new(area: Rect, default_state: A) -> Self {
-        Self {
-            view: PushButtonView::new(area),
-            abstraction: default_state,
-        }
-    }
-}
-
-// ------------------------------------------------------------------------
 // Event Loop Control interface
 //
 // NOTE: This is NOT a Controller.
@@ -267,4 +94,7 @@ impl<A: BoolViewAbstraction> PushButton<A> {
 pub trait EventLoopControl {
     fn approve_quit(&mut self);
 }
+
+// ------------------------------------------------------------------------
+// Boolean gadget components
 
