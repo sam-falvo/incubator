b58c8d3e68b486e5d32a8a9e522c832d67523819
Samuel A. Falvo II <kc5tja@arrl.net>
2023-12-31 14:01:23 -0800

    Statements can detect their various fields

--- compiler/hlxa/statement.c
+++ compiler/hlxa/statement.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "slice.h"
 #include "section.h"
@@ -7,6 +8,10 @@
 
 
 struct statement_desc {
+	struct slice_desc label_slice;
+	struct slice_desc mnemonic_slice;
+	struct slice_desc operand_slice;
+	int               errors;
 };
 
 
@@ -18,7 +23,10 @@ statement_alloc(void) {
 static statement_t
 statement_init(statement_t s) {
   if(s) {
-		memset(s, -1, sizeof(struct statement_desc));
+		memset(s, 0, sizeof(struct statement_desc));
+		slice_init(&s->label_slice);
+		slice_init(&s->mnemonic_slice);
+		slice_init(&s->operand_slice);
 	}
 	return s;
 }
@@ -36,26 +44,73 @@ statement_free(statement_t *ps) {
 	}
 }
 
+static void
+detect_field(section_t linebuf, size_t length, int *pstart, int *pend, slice_t slice) {
+	int start = *pstart, end = *pend;
+
+	if(start >= length) return;
+	if(end >= length) return;
+
+	if(!isspace(section_byte_at(linebuf, end))) {
+		while(
+			(end < length) &&
+			!isspace(section_byte_at(linebuf, end))
+		) ++end;
+
+		slice->start = start;
+		slice->end = end;
+
+		*pstart = end;
+		*pend = end;
+	}
+}
+
+static void
+skip_whitespace(section_t linebuf, size_t length, int *pstart, int *pend) {
+	int start = *pstart;
+	int end = *pend;
+
+	if(start >= length) return;
+	if(end >= length) return;
+
+  while(
+		(start < length) &&
+		(isspace(section_byte_at(linebuf, start)))
+	) ++ start;
+
+	*pstart = start;
+	*pend = start;
+}
+
 void
 statement_decode(section_t linebuf, statement_t out) {
+	int start = 0;
+	int end = 0;
+	size_t length = section_length(linebuf);
+
+	detect_field(linebuf, length, &start, &end, statement_borrow_label(out));
+	skip_whitespace(linebuf, length, &start, &end);
+	detect_field(linebuf, length, &start, &end, statement_borrow_mnemonic(out));
+	skip_whitespace(linebuf, length, &start, &end);
+	detect_field(linebuf, length, &start, &end, statement_borrow_operand(out));
 }
 
 int
 statement_errors(statement_t s) {
-	return 0;
+	return s->errors;
 }
 
 slice_t
 statement_borrow_label(statement_t s) {
-	return NULL;
+	return &s->label_slice;
 }
 
 slice_t
 statement_borrow_mnemonic(statement_t s) {
-	return NULL;
+	return &s->mnemonic_slice;
 }
 
 slice_t
 statement_borrow_operand(statement_t s) {
-	return NULL;
+	return &s->operand_slice;
 }
